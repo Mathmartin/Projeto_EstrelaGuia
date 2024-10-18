a4dfe5c55338218ccb379f149d34d46c7979dfe6
Mathmartin <87909772+Mathmartin@users.noreply.github.com>
2024-10-18 00:43:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function contactWhatsApp() {
+    window.open('https://wa.me/1234567890', '_blank');
+}
+
+function openContactForm() {
+    document.getElementById('contactForm').style.display = 'block';
+}
+
+function closeContactForm() {
+    document.getElementById('contactForm').style.display = 'none';
+}
